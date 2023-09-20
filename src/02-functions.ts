ceddb26e51ce5ed8550269724d43f602553918fc
Jack Aherne <jack.aherne03@gmail.com>
2023-09-20 10:21:43 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,6 +1,7 @@
 import {Friend, Colleague, Emailcontact } from './myTypes'
 import { friends, colleagues } from "./01-basics";
 
+// Return a string describing the new age of the friend (e.g. "Michael Gerber is now 32")
 function older(f: Friend) : string {
      f.age += 1
      return `${f.name} is now ${f.age}` 
@@ -8,6 +9,7 @@ function older(f: Friend) : string {
 
 console.log(older(friends[0]))
 
+// Return an array of strings describing the new ages of the friends (e.g. "Michael Gerber is now 32")
 function allOlder(friendsArray: Friend[]) {
     return friendsArray.map(friend => {
       friend.age += 1;
@@ -26,6 +28,7 @@ function highestExtension(cs: Colleague[]) {
   }
 console.log(highestExtension(colleagues.current));
 
+// Add a colleague to the current colleagues list (with a new extension number) and return the new colleague
 function addColleague(colleagueArray: Colleague[], name: string, department: string, email: string) {
   const highest = highestExtension(colleagues.current);
   const newExtension = highest.contact.extension + 1;
@@ -42,6 +45,7 @@ function addColleague(colleagueArray: Colleague[], name: string, department: str
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"))
 
+// Sort colleagues by name length (shortest first) and return an array of email contacts
 function sortColleagues(
   colleagues: Colleague[],
   sorter: (c1: Colleague, c2: Colleague) => number,
@@ -60,9 +64,21 @@ console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - 
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
 console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
 
+// Find friends by criteria (name starts with "Mo", age less than 35)
 function findFriends(friendsArray: Friend[], criteriaCallback: (friend: Friend) => boolean) {
   return friendsArray.filter(criteriaCallback).map((friend) => friend.name);
 }
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith("Mo")));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+// Add an interest to a friend (if they don't already have it)
+function addInterest(friend: Friend, interest: string) {
+  if (friend.interests === undefined) {
+    friend.interests = [];
+  }
+  friend.interests.push(interest);
+  return friend.interests;
+}
+
+console.log(addInterest(friends[1], "Politics"));
